963a651adc549c0a8095e90b4878d2f83b5808d5
Nicholas Maestrello Agiz <nicholas.agiz@seniorsolution.com.br>
2018-01-05 11:15:22 -0200

    corrigido problema cor do navbar

--- appReact/frontend/src/template/menu.jsx
+++ appReact/frontend/src/template/menu.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default props => (
-    <nav className='navbar nav-inverse bg-inverse'>
+    <nav className='navbar navbar-inverse bg-inverse'>
         <div className='container'>
             <div className='navbar-header'>
                 <a className='navbar-brand' href='#'>
